17b64fefc4e48593564b09a3db50b99341855241
Antonio Ricardo <antonio.eschola@gmail.com>
2022-11-13 09:27:35 -0400

    feat: add step 2 to challenge

--- main.go
+++ main.go
@@ -6,8 +6,12 @@ func main() {
 	slice := make([]int, 1, 10)
 	for i := 1; i <= 100; i++ {
 		if i%3 == 0 {
+			fmt.Println("Pin")
 			slice = append(slice, i)
+		} else if i%5 == 0 {
+			fmt.Println("Pan")
 		}
+		fmt.Println(i)
 	}
 	fmt.Println(slice)
 }
